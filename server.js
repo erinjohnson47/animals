e47b00810376197b26a89789b8306ec1a0a5916d
Jason Wong <jwong175@hotmail.com>
2019-07-17 14:41:39 -0700

    Complete animal page and include image links

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('index.ejs', {
         animals: Animals
-    })
-})
+    });
+});
+
+app.get('/animals/:id', (req, res) => {
+    res.render('show.ejs', {
+        animals: Animals[req.params.id]
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
-})
+});
 
 module.exports = app;
